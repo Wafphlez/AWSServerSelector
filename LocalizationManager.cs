38e0c37f5b2643d2cd408ab8cd1a62eb80d61b59
Wafphlez <wafphlez@gmail.com>
2025-10-09 18:00:38 +0300

    Fix (based release)

--- LocalizationManager.cs
+++ LocalizationManager.cs
@@ -13,7 +13,7 @@ namespace AWSServerSelector
 
         static LocalizationManager()
         {
-            _resourceManager = new ResourceManager("AWSServerSelector.Resources.Strings", typeof(LocalizationManager).Assembly);
+            _resourceManager = new ResourceManager("PingByDaylight.Resources.Strings", typeof(LocalizationManager).Assembly);
             _currentCulture = CultureInfo.CurrentCulture;
         }
 
